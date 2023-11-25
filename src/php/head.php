6f95f3def4e7e65f7051ff797379c20ca7a09f57
IlMattek <151029822+IlMattek@users.noreply.github.com>
2023-11-25 15:04:05 +0000

    UploadingHeadBodyandIndexPHP

---
+++ src/php/head.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="UTF-8">
+  <title>ArsenalFront</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/NavbarStyle.css">
+
+</head>
