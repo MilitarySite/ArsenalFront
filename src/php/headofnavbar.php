91334281164584010b9190a7be2b47c1a3dfbf42
IlMattek <151029822+IlMattek@users.noreply.github.com>
2023-11-25 16:50:39 +0000

    Renaming Navbar CSS and Testing Logo CSS

--- src/php/headofnavbar.php
+++ src/php/headofnavbar.php
@@ -2,6 +2,6 @@
   <meta charset="UTF-8">
   <title>ArsenalFront</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-<link rel="stylesheet" href="css/NavbarStyle.css">
+<link rel="stylesheet" href="css/NavbarStyleCopy.css">
 
 </head>
