4742fa6010964ac1590e19c545a86ee37058c7dc
IlMattek <151029822+IlMattek@users.noreply.github.com>
2023-11-26 14:09:00 +0000

    TryingToCorrectNavbar

--- src/php/menu.php
+++ src/php/menu.php
@@ -1,23 +1,24 @@
 <?php include('headofnavbar.php');?>
 <nav class="nav">
-<div class="fixed-nav-bar">
-    <div class="logo"><span>Arsenal</span>Front</div>
-    <input type="checkbox" id="menuButton" />
-    <div id="mainListDiv" class="main_list">
-        <ul class="navlinks">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </div>
-    <span class="navTrigger">
-        <i></i>
-        <i></i>
-        <i></i>
-    </span>
-</nav>
-
+        <div class="container">
+            <div class="logo">
+                <a href="#">ArsenalFront</a>
+            </div>
+            <div id="mainListDiv" class="main_list">
+                <ul class="navlinks">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <span class="navTrigger">
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+        </div>
+    </nav>
 <!-- Jquery needed -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="js/NavbarScript.js"></script>
