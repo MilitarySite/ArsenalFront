16f4dce13b7365bad0fea59cc9c79169c9461388
FlyBayTv <kevin.heinrich.b@gmail.com>
2023-11-28 08:15:36 +0100

    changed

--- Backend/src/php/Admin_Interface.php
+++ Backend/src/php/Admin_Interface.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-   <?php include('Admin_Head.php');?>
-</head> 
+    <head>
+        <?php include('Admin_Head.php');?>
+        <link href="src/css/AdminStyle.css" rel="stylesheet" />
+
+    </head> 
     <body class="sb-nav-fixed">
         <?php include('Admin_Body.php');?>
-        <?php include('src/TemporaryLogin.php');?>
+        <?php include('TemporaryLogin.php');?>
         <?php include('Admin_Footer.php');?>
                 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="src/js/scripts.js"></script>
+        <script src="src/js/scripts.js"></script> 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
         <script src="assets/demo/chart-area-demo.js"></script>
         <script src="assets/demo/chart-bar-demo.js"></script>
