b5e772c1d201b63f8a40ddc5fe658faba913a3c4
IlMattek <151029822+IlMattek@users.noreply.github.com>
2023-11-29 11:57:33 +0000

    Removing Old PHP Pages and Adding Admin Head

--- Backend/src/php/head.php
+++
@@ -1,6 +0,0 @@
-<head>
-  <meta charset="UTF-8">
-  <title>ArsenalFront</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-<link rel="stylesheet" href="css/style.css">
-</head>
