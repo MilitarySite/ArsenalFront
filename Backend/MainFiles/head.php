9ad770ce152a27a6ae565a1d6fe6db3afb4dfdbb
IlMattek <151029822+IlMattek@users.noreply.github.com>
2023-12-04 16:05:00 +0000

    AddingIconOnTopOfPage

--- Backend/MainFiles/head.php
+++ Backend/MainFiles/head.php
@@ -3,3 +3,4 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <!-- navbar css link  -->
 <link rel="stylesheet" href="../src/css/style.css">
+<link rel="shortcut icon" type="image/x-icon" href="img/Head_Logo.jpg">
