3636e3ae6975c3cb38e414c9ba88c6a098eead32
IlMattek <151029822+IlMattek@users.noreply.github.com>
2023-12-04 16:07:32 +0000

    CorrectingNavbar

--- Backend/MainFiles/navbar.php
+++ Backend/MainFiles/navbar.php
@@ -1,16 +1,19 @@
-<div class="container-fluid">
-    <div class="logo">
-      <img src ="src/img/Military_Army_Logo.jpg">
-    </div>
+<nav class="fixed-nav-bar logo span scrolled ">
+  <div class="container-fluid">
+    <!--<div class="logo">
+      <img src ="src/img/Head_Logo.jpg">
+    </div>-->
     <ul class="nav navbar-nav">
+    <li><img class="logo" src ="src/img/Head_Logo.jpg"></li>
       <li class="active"><a href="index.php">Home</a></li>
       <li><a href="#">Armi</a></li>
       <li><a target="_blank"  href="src/php/loginpage.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      <div>
-			<form role="search">
+      <li> 
+      <form role="search">
 				<input type="text" placeholder="Search...">
 				<a href=""></a>
-			</form>
-	    </div>
+                </form>
+      </li>
     </ul>
   </div>
+</nav>
--- Backend/index.php
+++ Backend/index.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" >
 <head>
-  <meta charset="UTF-8">
-  <title>ArsenalFront</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <!-- navbar css link  -->
-<link rel="stylesheet" href="src/css/style.css">
+  <?php include 'MainFiles/head.php'?>
 
 </head>
 <body>
