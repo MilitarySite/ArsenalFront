8005dcd405849d16754f7f17740eaa7794089aa3
IlMattek <151029822+IlMattek@users.noreply.github.com>
2023-12-04 15:27:41 +0000

    RE-adding Files Codes

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<div class="container-fluid">
+    <div class="logo">
+      <img src ="src/img/Military_Army_Logo.jpg">
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="#">Armi</a></li>
+      <li><a target="_blank"  href="src/php/loginpage.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <div>
+			<form role="search">
+				<input type="text" placeholder="Search...">
+				<a href=""></a>
+			</form>
+	    </div>
+    </ul>
+  </div>
---
+++ weaponhead.php
@@ -0,0 +1,5 @@
+<meta charset="UTF-8">
+  <title>ArsenalFront</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <!-- navbar css link  -->
+<link rel="stylesheet" href="TemporaryWeaponsPageCSS.css">
