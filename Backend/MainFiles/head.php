e2eea75d1924f4c1da4490862968f8ac82cdc0be
FlyBayTv <kevin.heinrich.b@gmail.com>
2023-12-06 13:00:39 +0100

    add new index template because matte can't code

--- Backend/MainFiles/head.php
+++
@@ -1,6 +0,0 @@
-<meta charset="UTF-8">
-  <title>ArsenalFront</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> <!-- navbar css link  -->
-<link rel="stylesheet" href="../src/css/style.css">
-<link rel="shortcut icon" type="image/x-icon" href="../src/img/Head_Logo.jpg">
