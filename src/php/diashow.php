f88cd1e4cfe1fdfcb4a805f9e995b6105609b071
IlMattek <abbassoplay4@gmail.com>
2024-01-20 15:51:40 +0100

    adding js files, main section and changing text

--- src/php/diashow.php
+++ src/php/diashow.php
@@ -32,9 +32,13 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
 
     <!-- Stylesheet -->
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/styles.css" />
     <link rel="stylesheet" href="../css/search.css" />
-    <link rel="stylesheet" href="../css/animatedfooter.css"/>
+    <link rel="stylesheet" href="../css/animatedfooter.css" />
 </head>
 <body>
 
@@ -70,21 +74,20 @@
 
 <header class="masthead">
         <div class="container">
-            <div class="masthead-heading text-uppercase fs-title">Scegli l'arma che preferisci!</div>  
-        </div>
-    </header>
-    <section class="page-section bg-dark">
-            <div class="container">
-                <div class="text-center">
+        <div class="masthead-heading text-uppercase">Scegli l'arma che preferisci!</div>
+            <div class="text-center">
                     <h3 class="section-subheading text-muted fw-bold mb-5 mb-lg-0">All'interno di ognuna delle sezioni relative alle armi, troverai 5
                         modelli diversi. Per ciascuno di essi, troverai descrizioni precise su tutte le caratteristiche dell'arma. Speriamo che queste 
                         informazioni possano ritornarvi utili :)
                     </h3>
-                </div>
-            </div>
-        </section>    
-
+                </div>  
+        </div>
+    </header>
+    <main>
+    
     <div id="diashow-container"></div>
+
+</main>
 
 <footer class="footer py-4">
         <div class="container">
@@ -172,6 +175,23 @@
 
     // Imposta un intervallo per cambiare l'immagine ogni 2 secondi
     setInterval(cambiaImmagine, 5000);
+    
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core theme JS-->
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/popper.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/main.js"></script>
+    <script src="../js/scripts.js"></script>
+
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </script>
 
 </body>
