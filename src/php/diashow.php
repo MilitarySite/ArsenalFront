12de542b38d56dc7cee477364b9d54574ae77adc
IlMattek <abbassoplay4@gmail.com>
2024-01-20 15:24:19 +0100

    fixing minor problem index add footer to diashow

--- src/php/diashow.php
+++ src/php/diashow.php
@@ -60,15 +60,40 @@
                         </form>
                     </div>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="page/WeaponPage.html">Armi</a></li>
-                <li class="nav-item"><a class="nav-link" href="php/login.php">Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="../index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="WeaponPage.php">Armi</a></li>
+                <li class="nav-item"><a class="nav-link" href="loginpage.php">Login</a></li>
             </ul>
         </div>
     </div>
 </nav>
 
 
+
+<footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <!--<div class="col-lg-4 text-lg-start">Copyright &copy; Porro</div>-->
+                <div class="member" id="member1" onclick="sendEmail('sappia.fulvio@polotecnologicoimperiese.it')">
+                    <img src="assets/img/team/1.jpg" alt="Foto Sappippa">
+                    <div class="overlayz"></div>
+                  </div>
+                  <div class="member" id="member2" onclick="sendEmail('heinrich.kevin@polotecnologicoimperiese.it')">
+                    <img src="assets/img/team/2.jpg" alt="Foto Tedesco Heineken">
+                    <div class="overlayz"></div>
+                  </div>
+                  <div class="member" id="member3" onclick="sendEmail('porro.matteo@polotecnologicoimperiese.it')">
+                    <img src="assets/img/team/3.jpg" alt="Foto Re Dei Porri">
+                    <div class="overlayz"></div>
+                  </div>
+                  <div class="member" id="member4" onclick="sendEmail('contri.marco@polotecnologicoimperiese.it')">
+                    <img src="assets/img/team/1.jpg" alt="Foto di quello che non inContri">
+                    <div class="overlayz"></div>
+                  </div>
+            </div>
+        </div>
+    </footer>
+</body>
 <div id="diashow-container"></div>
 
 <script>
