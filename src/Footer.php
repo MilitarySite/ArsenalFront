0bac1b3fbc57363c533abfd9499b9baee9ef49f7
IlMattek <abbassoplay4@gmail.com>
2024-01-22 16:47:32 +0100

    footer and added images

--- src/Footer.php
+++ src/Footer.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Your Website</title>
-  <link rel="stylesheet" href="css/animatedfooter.css"> <!-- Assicurati che il percorso del tuo file CSS sia corretto -->
-
-  <!-- Altri link ai tuoi script e stili -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-
   <div class="footer-clean">
     <footer>
       <div class="container">
