eab02026962a2a65bdef14d57a16728056a16345
IlMattek <abbassoplay4@gmail.com>
2024-01-22 17:53:56 +0100

    Inserting summary

--- src/php/File2.php
+++ src/php/File2.php
@@ -225,6 +225,20 @@
             </div>
         </div>
     </div>
+    <section class="page-section bg-dark">
+            <div class="container">
+                <div class="text-center">
+                    <h3 class="section-subheading text-muted fw-bold mb-5 mb-lg-0">Sommario Informazioni Arma:</h3>
+                    <br>
+                    <h3 class="section-subheading text-muted fw-bold mb-5 mb-lg-0"> <?php echo $marca ?></h3>
+                    <br>
+                    <h3 class="section-subheading text-muted fw-bold mb-5 mb-lg-0"> <?php echo $modello ?></h3>
+                    <br>
+                    <h3 class="section-subheading text-muted fw-bold mb-5 mb-lg-0"> <?php echo $stato ?></h3>
+                    <br>
+                </div>
+            </div>
+        </section>    
 
     <footer>
         <?php include '../Footer.php';?>
